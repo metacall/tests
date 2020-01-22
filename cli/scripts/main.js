3408d2e788a1bc028b1d7275fd2f23cbb770f8c4
viferga <vic798@gmail.com>
2020-01-22 14:21:26 -0900

    All tests working properly with installer and ports (python / nodejs).

--- cli/scripts/main.js
+++ cli/scripts/main.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-// const { metacall, metacall_load_from_file } = require('metacall');
+const { metacall, metacall_load_from_file } = require('metacall');
 
 module.exports = {
 	hello: function(a) {
