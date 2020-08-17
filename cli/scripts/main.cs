75a2cbebda6ddc2f5d0a7fc1722b6b83baeb3a8a
viferga <vic798@gmail.com>
2020-08-17 12:54:47 -0800

    Add base for C# tests.

---
+++ cli/scripts/main.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Scripts
+{
+	public class Program
+	{
+		public static void SayHello()
+		{
+			Console.WriteLine("HELLO");
+		}
+
+		public static void Say(string text)
+		{
+			Console.WriteLine(text);
+		}
+	}
+}
